a8fe2046948ecf2c0fdc112b70b49b9fd26fddfc
Alan Colon <github@alanc.net>
2015-09-25 10:47:30 -0600

    Fixed typo in Java

--- Code.java
+++ Code.java
@@ -16,10 +16,10 @@ public class Code {
     }
 
     // Calculate the golden ratio.
-    // Given two numbers, a and b, the ratio is a / b.
-    // Let c = a + b, then the ratio b / c is closer to the golden ratio.
-    // Let d = b + c, then the ratio c / d is closer to the golden ratio.
-    // Let e = c + d, then the ratio d / e is closer to the golden ratio.
+    // Given two numbers a and b with a > b > 0, the ratio is b / a.
+    // Let c = a + b, then the ratio c / b is closer to the golden ratio.
+    // Let d = b + c, then the ratio d / c is closer to the golden ratio.
+    // Let e = c + d, then the ratio e / d is closer to the golden ratio.
     // If you continue this process, the result will trend towards the golden ratio.
     public static Double goldenRatio(Double a, Double b) {
         throw new RuntimeException("Not Implemented");
