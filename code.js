1df94a5ca5e5230b8b2236611fdb31ef4c6be484
Matt Weaver <ezweave@gmail.com>
2018-09-18 08:26:55 -0600

    close

--- code.js
+++ code.js
@@ -3,6 +3,35 @@ const capitalizeWord = (word) =>
     (index === 0) ? letter.toUpperCase() : letter
   ).join('')
 
+const quad = (a, b, c, sign) =>
+  (
+    (-1 * b) + (
+      sign * Math.sqrt(
+        Math.pow(b, 2) - (4 * a * c)
+      )
+    )
+  )/(2 * a)
+
+const quadratic = (a, b, c) =>
+  [
+    quad(a, b, c, 1),
+    quad(a, b, c, -1)
+  ]
+
+const positiveQuad = (a, b, c) =>
+  quadratic(a, b, c).filter(x => x > 0).pop()
+
+// this is an exact solution for phi, but we can use it to test our estimates
+const phi = positiveQuad(1, -1, -1)
+
+const checkRange = (ratio) =>
+  Math.abs(ratio - phi) < 0.00001 
+
+const gold = (a, b) =>
+  Math.sign.apply([a, b]) ?
+    checkRange((a+b)/b) ? (a+b)/b : gold(b, a + b)
+: undefined
+
 var code = {
     // Returns "Hello World!"
     helloWorld: function() {
@@ -34,14 +63,19 @@ var code = {
     // Let d = b + c, then the ratio d / c is closer to the golden ratio. 
     // Let e = c + d, then the ratio e / d is closer to the golden ratio.
     // If you continue this process, the result will trend towards the golden ratio.
-    goldenRatio: function(a, b) {
-        throw new Error("Not Implemented");
-    },
+    goldenRatio: gold, 
 
     // Give the nth Fibonacci number
     // Starting with 0, 1, 1, 2, ... a Fibonacci number is the sum of the previous two.
     fibonacci: function(n) {
-        throw new Error("Not Implemented");
+      return new Array(n).fill(0).reduce((fibb, curr, i) => 
+        fibb.concat([{
+          a: fibb[i].b,
+          b: fibb[i].a + fibb[i].b
+        }]), [{
+          a: 0,
+          b: 1
+        }]).pop().a
     },
 
     // Give the square root of a number
